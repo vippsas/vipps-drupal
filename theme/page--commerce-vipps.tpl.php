5e6a76983251ae6566d794ee9d56a81dfb9ce73a
Jakub Piasecki <jakub@nymedia.no>
2016-09-20 16:03:29 +0200

    Add styling

--- theme/page--commerce-vipps.tpl.php
+++ theme/page--commerce-vipps.tpl.php
@@ -1,6 +1,9 @@
-<?php if ($logo): ?>
-    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-<?php endif; ?>
-<?php print $messages; ?>
-<?php print render($page['content']); ?>
-
+<div class="page-wrapper">
+  <div class="content-wrapper">
+    <?php if ($logo): ?>
+      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="logo" />
+    <?php endif; ?>
+    <?php print $messages; ?>
+    <?php print render($page['content']); ?>
+  </div>
+</div>
