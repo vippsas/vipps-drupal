2532ce8fc52fa42e576bc4a5aff01e6c074a5dbc
Jakub Piasecki <jakub@nymedia.no>
2016-09-21 15:46:15 +0200

    Do ajax call async

--- assets/commerce_vipps.js
+++ assets/commerce_vipps.js
@@ -4,14 +4,14 @@
             var commerce_vipps = jQuery.ajax({
                 type: "GET",
                 url: settings.basePath + "commerce_vipps/" + settings.commerce_vipps.transaction + "/transaction/" + settings.commerce_vipps.payment_redirect_key,
-                async: false,
+                async: true,
                 success: function(data, textStatus, XMLHttpRequest){
                     location.reload();
                 },
                 error:function (xhr, ajaxOptions, thrownError){
                     setTimeout(function () {
                         Drupal.attachBehaviors(context, settings);
-                    }, 5000);
+                    }, 1000);
                 }
             });
         }
